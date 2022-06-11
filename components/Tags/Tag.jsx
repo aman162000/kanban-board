dfa5922f724e679cc88c08dd5621523ec3a57815
aman162000 <borse.aman@rediffmail.com>
2022-06-11 17:12:21 +0530

    tags improved

--- components/Tags/Tag.jsx
+++ components/Tags/Tag.jsx
@@ -4,9 +4,7 @@ const Tag = (props) => {
   return (
     // <div className='tag'>
     <sapn className="tag" style={{ backgroundColor: `${props?.color}` }}>
-      {props?.tagName.length >= 10
-        ? props.tagName.slice(0, 6) + "..."
-        : props.tagName || ""}
+      {props?.tagName}
     </sapn>
     // </div>
   );
